e9a6229f5b3cf5dd08814ab6ea18e22ea875a810
Sandeep Jha <91358961+ft-jsandeep@users.noreply.github.com>
2022-02-03 11:17:01 +0530

    ExpensesFilter component added, value passed up to expenses component; Component structure:- App ---> Expenses ---> (ExpensesFilter) + (ExpensesList).

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 export default function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2022");
+  const filterChangeHandler = (filterYear) => {
+    setFilteredYear(filterYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilterYear={filterChangeHandler}
+      />
       <ExpensesList items={props.expenses} />
     </Card>
   );
